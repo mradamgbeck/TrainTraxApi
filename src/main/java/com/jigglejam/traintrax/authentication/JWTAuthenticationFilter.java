f53b533b5b8f0123bc88304bace77b17c2bcc7ef
mradamgbeck <mradamgbeck@gmail.com>
2020-04-24 14:03:04 -0400

    added logging

--- src/main/java/com/jigglejam/traintrax/authentication/JWTAuthenticationFilter.java
+++ src/main/java/com/jigglejam/traintrax/authentication/JWTAuthenticationFilter.java
@@ -62,11 +62,14 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                                             Authentication auth) throws IOException, ServletException {
         User user = (User) auth.getPrincipal();
         ApplicationUser appUser = applicationUserRepository.findByUsername(user.getUsername());
+        System.out.println("Logging in User: " + appUser.getUsername());
+        System.out.println("Role: " + appUser.getRole());
         String token = JWT.create()
                 .withSubject(((User) auth.getPrincipal()).getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(HMAC512(SECRET.getBytes()));
         res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
         res.addHeader("Role", appUser.getRole());
+        System.out.println("Response: " + res);
     }
 }
