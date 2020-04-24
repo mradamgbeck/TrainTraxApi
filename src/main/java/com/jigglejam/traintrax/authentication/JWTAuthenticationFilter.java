3f38b2fc936804f9fc20fabf89f6bb9fc499be88
mradamgbeck <mradamgbeck@gmail.com>
2020-04-24 12:39:55 -0400

    login now returns role

--- src/main/java/com/jigglejam/traintrax/authentication/JWTAuthenticationFilter.java
+++ src/main/java/com/jigglejam/traintrax/authentication/JWTAuthenticationFilter.java
@@ -2,7 +2,10 @@ package com.jigglejam.traintrax.authentication;
 
 import com.auth0.jwt.JWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jigglejam.traintrax.user.ApplicationUser;
 import com.jigglejam.traintrax.user.ApplicationUserDto;
+import com.jigglejam.traintrax.user.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -22,6 +25,8 @@ import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static com.jigglejam.traintrax.constants.SecurityConstants.*;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
     private ObjectMapper objectMapper;
     private AuthenticationManager authenticationManager;
 
@@ -55,11 +60,13 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                                             HttpServletResponse res,
                                             FilterChain chain,
                                             Authentication auth) throws IOException, ServletException {
-
+        User principal = (User) auth.getPrincipal();
+        ApplicationUser appUser = applicationUserRepository.findByUsername(principal.getUsername());
         String token = JWT.create()
-                .withSubject(((User) auth.getPrincipal()).getUsername())
+                .withSubject(principal.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(HMAC512(SECRET.getBytes()));
         res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+        res.addHeader("Role", appUser.getRole());
     }
 }
